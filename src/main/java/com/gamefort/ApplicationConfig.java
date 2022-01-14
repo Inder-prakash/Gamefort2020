2269c3f013146155d11a7862174501a3a7eb06de
inder <inder.prakash@hotmail.com>
2022-01-14 21:17:55 +0530

    fixed

--- src/main/java/com/gamefort/ApplicationConfig.java
+++ src/main/java/com/gamefort/ApplicationConfig.java
@@ -6,16 +6,12 @@ import java.util.ArrayList;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.mongodb.MongoDbFactory;
 import org.springframework.data.mongodb.core.MongoTemplate;
-import org.springframework.data.mongodb.core.SimpleMongoDbFactory;
 import org.springframework.web.multipart.commons.CommonsMultipartResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-
-import com.mongodb.MongoClient;
-import com.mongodb.MongoClientURI;
-import com.mongodb.MongoCredential;
-import com.mongodb.ServerAddress;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoDatabase;
 
 @Configuration
 @ComponentScan(basePackages = {"com.gamefort"})
@@ -23,39 +19,26 @@ import com.mongodb.ServerAddress;
 
 public class ApplicationConfig {
 	
-	public @Bean MongoClient mongoClient() throws UnknownHostException {
-        return new MongoClient(
-        		new MongoClientURI("mongodb+srv://gamer:gamer123@gamefort.iqleq.mongodb.net/gamefort?"
-        				+ "retryWrites=true&w=majority"));
+	@Bean
+	public MongoClient getMongoClient() {
+		MongoClient mongoClient = MongoClients.create("mongodb+srv://gamer:gamer123@gamefort.iqleq.mongodb.net/gamefort?retryWrites=true&w=majority");
+		System.out.println("Connecting with Mongo Client ..............................");
+		return mongoClient;
+	}
+
+	
+	@Bean
+	public MongoDatabase getNameDatabase() {
+		
+		MongoDatabase database = getMongoClient().getDatabase("gamefort");
+		System.out.println("Connection Establish ..............................");
+		return database;
 	}
 	
 	
-//	public @Bean MongoClient mongoClient() throws UnknownHostException {
-//        return new MongoClient(new ServerAddress("ds239988.mlab.com",39988), new ArrayList<MongoCredential>() {
-//            {
-//                add(MongoCredential.createCredential("inder", "gamefort", "inder@123".toCharArray()));
-//            }
-//        });
-//    }
-	
 	@Bean
-    public MongoDbFactory mongoDbFactory() throws Exception {
-		return new SimpleMongoDbFactory(mongoClient(), "gamefort");
-    }
- 
-    @Bean
-    public MongoTemplate mongoTemplate() throws Exception {
-        MongoTemplate mongoTemplate = new MongoTemplate(mongoDbFactory());
-        return mongoTemplate;
-    }
-    
-//    @Bean(name = "multipartResolver")
-//	public CommonsMultipartResolver createMultipartResolver() {
-//		
-//		System.out.println("Multpart ASD Resolver");
-//		
-//	    CommonsMultipartResolver resolver=new CommonsMultipartResolver();
-//	    resolver.setDefaultEncoding("utf-8");
-//	    return resolver;
-//	}
+	public MongoTemplate mongoTemplate() throws Exception {
+		MongoTemplate mgo = new MongoTemplate(getMongoClient(), getNameDatabase().getName());		
+		return mgo;
+	}
 }
